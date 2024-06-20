7be0112000ae25add99a70fd23c9fbccd0ebe088
Ian Scrol <imscrol@gmail.com>
2024-06-20 16:34:05 -0500

    Add help button

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import './App.css';
 import Title from "./components/title/Title";
+import HelpButton from './components/help-button/HelpButton';
 
 function App() {
   return (
     <div>
+      <HelpButton/>
       <Title/>
     </div>
   )
---
+++ src/components/help-button/HelpButton.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from './HelpButton.module.css'
+
+const HelpButton = () => {
+  return (
+    <button className={styles.helpBtn}>Help</button>
+  )
+}
+
+export default HelpButton
