168ef6496925770142578450ae29558084f4b9f6
Ian Scrol <imscrol@gmail.com>
2024-06-20 15:31:15 -0500

    Initial commit and made title

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css';
+import Title from "./components/title/Title";
+
+function App() {
+  return (
+    <div>
+      <Title/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/title/Title.tsx
@@ -0,0 +1,12 @@
+
+import styles from './Title.module.css'
+
+const Title = () => {
+  return (
+    <div className={styles.title}>
+      <h1>Fantasy Basketball Calculator</h1>
+    </div>
+  )
+}
+
+export default Title
